7f74cb139d70ee61c9358bce51f8b5896d15b418
shopmylabel <shopmylabel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-24 04:03:50 +0000

    Version 1.1 of plugin from Jeff.
    git-svn-id: https://plugins.svn.wordpress.org/shop-my-label-media-marketplace-engine/trunk@657778 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sml_cart_dev.php
@@ -0,0 +1,81 @@
+<?php
+
+
+
+	$siteUrl = get_home_url();
+
+
+
+    	$siteUrl = str_replace('http://','',$siteUrl);
+
+
+
+    	$siteUrl = str_replace('https://','',$siteUrl);
+
+
+
+	if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
+
+
+
+    		$redirect = "https://".$siteUrl."/cart";
+
+
+
+    		header("Location: $redirect");
+
+
+
+	}
+
+
+
+	// default values for Shopping Cart
+
+
+
+	$smlBaseUrl = 'https://www.shopmywindow.com/';
+
+
+
+ 	get_header( $name ); 
+
+
+
+ 	$smlCartUrl = '/embed/shoppingcart'
+
+
+
+ ?> 
+
+
+
+<script type="text/javascript" >
+
+
+
+	document.title = "<?php bloginfo('name')  ?> | Shopping Cart";
+
+
+
+</script>
+
+
+
+<div id="smlCartClearDiv">
+
+
+
+	<iframe id="smlIframeBase" class="smlCartIframe" src="<?php echo $smlBaseUrl.$smlCartUrl ?>" frameborder="0" scrolling="auto" seamless="seamless" marginheight="0" marginwidth="0"></iframe>
+
+
+
+</div>
+
+
+
+<link href="/wp-content/plugins/sml_shopkeeper/sml_shopkeeper.css" rel="stylesheet" type="text/css" />
+
+
+
+<?php get_footer( $name ); ?> 
